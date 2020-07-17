32d5901f4b3afe4cca95aa3cf8f9eae103a74d5b
AlterMann <MoskaletsAndrey@yandex.ru>
2020-07-17 15:36:19 +0300

    rollback products -> categories -> brands -> pictures -> pictures_data

--- shop-database/src/main/resources/db/changelog/v-1.0/01-drop-user-and-product-and-role-tables.sql
+++ shop-database/src/main/resources/db/changelog/v-1.0/01-drop-user-and-product-and-role-tables.sql
@@ -1,59 +1,55 @@
-ALTER TABLE `roles` DROP KEY `UK_ofx66keruapi6vyqpv6f2or37`;
+-- users -> users_roles -> roles
+    ALTER TABLE `roles` DROP KEY `UK_ofx66keruapi6vyqpv6f2or37`;
 GO
 
-ALTER TABLE `users_roles` DROP FOREIGN KEY `FKj6m8fwv7oqv74fcehir1a9ffy`;
+    ALTER TABLE `users_roles` DROP FOREIGN KEY `FKj6m8fwv7oqv74fcehir1a9ffy`;
 GO
 
-ALTER TABLE `users_roles` DROP FOREIGN KEY `FK2o0jvgh89lemvvo17cbqvdxaa`;
+    ALTER TABLE `users_roles` DROP FOREIGN KEY `FK2o0jvgh89lemvvo17cbqvdxaa`;
 GO
 
-DROP TABLE `roles`;
+    DROP TABLE `roles`;
 GO
 
-DROP TABLE `users_roles`;
+    DROP TABLE `users_roles`;
 GO
 
-DROP TABLE `users`;
+    DROP TABLE `users`;
 GO
 
---     alter table roles
---        drop index UK_ofx66keruapi6vyqpv6f2or37;
---     GO
---
---     alter table users_roles
---        drop constraint FKj6m8fwv7oqv74fcehir1a9ffy;
---     GO
---
---     alter table users_roles
---        drop constraint FK2o0jvgh89lemvvo17cbqvdxaa;
---     GO
---
---     alter table categories
---        drop index UK_t8o6pivur7nn124jehx7cygw5;
---     GO
---
---     alter table products_categories
---        drop constraint FKd112rx0alycddsms029iifrih;
---     GO
---
---     alter table products_categories
---        drop constraint FKlda9rad6s180ha3dl1ncsp8n7;
---     GO
---
---     drop table roles;
---     GO
---
---     drop table users;
---     GO
---
---     drop table users_roles;
---     GO
---
---     drop table categories;
---     GO
---
---     drop table products;
---     GO
---
---     drop table products_categories;
---     GO
+-- products -> categories -> brands -> pictures -> pictures_data
+    ALTER TABLE `brands` DROP KEY `UK_oce3937d2f4mpfqrycbr0l93m`;
+GO
+
+    ALTER TABLE `categories` DROP KEY `UK_t8o6pivur7nn124jehx7cygw5`;
+GO
+
+    ALTER TABLE `pictures` DROP FOREIGN KEY `FKe9cv52k04xoy6cj8xy308gnw3`;
+GO
+
+    ALTER TABLE `pictures` DROP FOREIGN KEY `FK43hu51t487tsmo7tltxmdx9br`;
+GO
+
+    ALTER TABLE `pictures` DROP KEY `UK_ehsu2tyinopypjox1ijxt3g3c`;
+GO
+
+    ALTER TABLE `products` DROP FOREIGN KEY `FKa3a4mpsfdf4d2y6r8ra3sc8mv`;
+GO
+
+    ALTER TABLE `products` DROP FOREIGN KEY `FKog2rp4qthbtt2lfyhfo32lsw9`;
+GO
+
+    DROP TABLE `brands`;
+GO
+
+    DROP TABLE `categories`;
+GO
+
+    DROP TABLE `pictures_data`;
+GO
+
+    DROP TABLE `pictures`;
+GO
+
+    DROP TABLE `products`;
+GO
