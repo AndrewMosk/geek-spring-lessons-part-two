7dcb52a5c7842a832ad5bc596503d9bc4ffcb477
AlterMann <MoskaletsAndrey@yandex.ru>
2020-07-17 14:28:29 +0300

    update и rollback таблицы users, users_roles, roles

--- shop-database/src/main/resources/db/changelog/v-1.0/01-create-user-and-product-and-role-tables.sql
+++ shop-database/src/main/resources/db/changelog/v-1.0/01-create-user-and-product-and-role-tables.sql
@@ -1,44 +1,44 @@
-create table brands (
-       id bigint not null auto_increment,
-        name varchar(255) not null,
-        primary key (id)
-    ) engine=InnoDB
-GO 
-    
-    create table categories (
-       id bigint not null auto_increment,
-        name varchar(255) not null,
-        parent_id bigint,
-        primary key (id)
-    ) engine=InnoDB
-GO
-
-    create table pictures (
-       id bigint not null auto_increment,
-        content_type varchar(255) not null,
-        name varchar(255) not null,
-        picture_data_id bigint not null,
-        product_id bigint not null,
-        primary key (id)
-    ) engine=InnoDB
-GO
-
-    create table pictures_data (
-       id bigint not null auto_increment,
-        data longblob not null,
-        primary key (id)
-    ) engine=InnoDB
-GO
-
-    create table products (
-       id bigint not null auto_increment,
-        cost decimal(19,2),
-        name varchar(64),
-        brand_id bigint not null,
-        category_id bigint not null,
-        primary key (id)
-    ) engine=InnoDB
-GO
+-- create table brands (
+--        id bigint not null auto_increment,
+--         name varchar(255) not null,
+--         primary key (id)
+--     ) engine=InnoDB
+-- GO
+--
+--     create table categories (
+--        id bigint not null auto_increment,
+--         name varchar(255) not null,
+--         parent_id bigint,
+--         primary key (id)
+--     ) engine=InnoDB
+-- GO
+--
+--     create table pictures (
+--        id bigint not null auto_increment,
+--         content_type varchar(255) not null,
+--         name varchar(255) not null,
+--         picture_data_id bigint not null,
+--         product_id bigint not null,
+--         primary key (id)
+--     ) engine=InnoDB
+-- GO
+--
+--     create table pictures_data (
+--        id bigint not null auto_increment,
+--         data longblob not null,
+--         primary key (id)
+--     ) engine=InnoDB
+-- GO
+--
+--     create table products (
+--        id bigint not null auto_increment,
+--         cost decimal(19,2),
+--         name varchar(64),
+--         brand_id bigint not null,
+--         category_id bigint not null,
+--         primary key (id)
+--     ) engine=InnoDB
+-- GO
 
     create table roles (
        id bigint not null auto_increment,
@@ -66,45 +66,45 @@ GO
     ) engine=InnoDB
 GO
 
-    alter table brands
-       add constraint UK_oce3937d2f4mpfqrycbr0l93m unique (name)
-GO
-
-    alter table categories
-       add constraint UK_t8o6pivur7nn124jehx7cygw5 unique (name)
-GO
-
-    alter table pictures
-       add constraint UK_ehsu2tyinopypjox1ijxt3g3c unique (picture_data_id)
-GO
+--     alter table brands
+--        add constraint UK_oce3937d2f4mpfqrycbr0l93m unique (name)
+-- GO
+--
+--     alter table categories
+--        add constraint UK_t8o6pivur7nn124jehx7cygw5 unique (name)
+-- GO
+--
+--     alter table pictures
+--        add constraint UK_ehsu2tyinopypjox1ijxt3g3c unique (picture_data_id)
+-- GO
 
     alter table roles
        add constraint UK_ofx66keruapi6vyqpv6f2or37 unique (name)
 GO
 
-    alter table pictures
-       add constraint FKe9cv52k04xoy6cj8xy308gnw3
-       foreign key (picture_data_id)
-       references pictures_data (id)
-GO
-
-    alter table pictures
-       add constraint FK43hu51t487tsmo7tltxmdx9br
-       foreign key (product_id)
-       references products (id)
-GO
-
-    alter table products
-       add constraint FKa3a4mpsfdf4d2y6r8ra3sc8mv
-       foreign key (brand_id)
-       references brands (id)
-GO
-
-    alter table products
-       add constraint FKog2rp4qthbtt2lfyhfo32lsw9
-       foreign key (category_id)
-       references categories (id)
-GO
+--     alter table pictures
+--        add constraint FKe9cv52k04xoy6cj8xy308gnw3
+--        foreign key (picture_data_id)
+--        references pictures_data (id)
+-- GO
+--
+--     alter table pictures
+--        add constraint FK43hu51t487tsmo7tltxmdx9br
+--        foreign key (product_id)
+--        references products (id)
+-- GO
+--
+--     alter table products
+--        add constraint FKa3a4mpsfdf4d2y6r8ra3sc8mv
+--        foreign key (brand_id)
+--        references brands (id)
+-- GO
+--
+--     alter table products
+--        add constraint FKog2rp4qthbtt2lfyhfo32lsw9
+--        foreign key (category_id)
+--        references categories (id)
+-- GO
 
     alter table users_roles
        add constraint FKj6m8fwv7oqv74fcehir1a9ffy
--- shop-database/src/main/resources/db/changelog/v-1.0/01-drop-user-and-product-and-role-tables.sql
+++ shop-database/src/main/resources/db/changelog/v-1.0/01-drop-user-and-product-and-role-tables.sql
@@ -1,41 +1,59 @@
-    alter table roles
-       drop index UK_ofx66keruapi6vyqpv6f2or37;
-    GO
+ALTER TABLE `roles` DROP KEY `UK_ofx66keruapi6vyqpv6f2or37`;
+GO
 
-    alter table users_roles
-       drop constraint FKj6m8fwv7oqv74fcehir1a9ffy;
-    GO
+ALTER TABLE `users_roles` DROP FOREIGN KEY `FKj6m8fwv7oqv74fcehir1a9ffy`;
+GO
 
-    alter table users_roles
-       drop constraint FK2o0jvgh89lemvvo17cbqvdxaa;
-    GO
+ALTER TABLE `users_roles` DROP FOREIGN KEY `FK2o0jvgh89lemvvo17cbqvdxaa`;
+GO
 
-    alter table categories
-       drop index UK_t8o6pivur7nn124jehx7cygw5;
-    GO
+DROP TABLE `roles`;
+GO
 
-    alter table products_categories
-       drop constraint FKd112rx0alycddsms029iifrih;
-    GO
+DROP TABLE `users_roles`;
+GO
 
-    alter table products_categories
-       drop constraint FKlda9rad6s180ha3dl1ncsp8n7;
-    GO
+DROP TABLE `users`;
+GO
 
-    drop table roles;
-    GO
-
-    drop table users;
-    GO
-
-    drop table users_roles;
-    GO
-
-    drop table categories;
-    GO
-
-    drop table products;
-    GO
-
-    drop table products_categories;
-    GO
+--     alter table roles
+--        drop index UK_ofx66keruapi6vyqpv6f2or37;
+--     GO
+--
+--     alter table users_roles
+--        drop constraint FKj6m8fwv7oqv74fcehir1a9ffy;
+--     GO
+--
+--     alter table users_roles
+--        drop constraint FK2o0jvgh89lemvvo17cbqvdxaa;
+--     GO
+--
+--     alter table categories
+--        drop index UK_t8o6pivur7nn124jehx7cygw5;
+--     GO
+--
+--     alter table products_categories
+--        drop constraint FKd112rx0alycddsms029iifrih;
+--     GO
+--
+--     alter table products_categories
+--        drop constraint FKlda9rad6s180ha3dl1ncsp8n7;
+--     GO
+--
+--     drop table roles;
+--     GO
+--
+--     drop table users;
+--     GO
+--
+--     drop table users_roles;
+--     GO
+--
+--     drop table categories;
+--     GO
+--
+--     drop table products;
+--     GO
+--
+--     drop table products_categories;
+--     GO
