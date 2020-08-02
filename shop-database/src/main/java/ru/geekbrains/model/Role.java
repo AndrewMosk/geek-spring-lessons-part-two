4ada5c3dfdea6ddf383837fbf33809e35a2798df
AlterMann <MoskaletsAndrey@yandex.ru>
2020-08-02 14:43:49 +0300

    пример из телеги

--- shop-database/src/main/java/ru/geekbrains/model/Role.java
+++ shop-database/src/main/java/ru/geekbrains/model/Role.java
@@ -63,4 +63,9 @@ public class Role implements Serializable {
     public int hashCode() {
         return Objects.hash(name);
     }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + "]";
+    }
 }
--- spring-integration-demo/src/main/java/ru/geekbrains/ImportConfiguration.java
+++ spring-integration-demo/src/main/java/ru/geekbrains/ImportConfiguration.java
@@ -2,20 +2,25 @@ package ru.geekbrains;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.ConsumerEndpointSpec;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
-import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.transformer.FileToStringTransformer;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
+//import org.springframework.integration.jdbc.JdbcMessageHandler;
+import org.springframework.integration.jpa.dsl.Jpa;
+import org.springframework.integration.jpa.dsl.JpaUpdatingOutboundEndpointSpec;
+import org.springframework.integration.jpa.support.PersistMode;
+import ru.geekbrains.model.Brand;
 
+import javax.persistence.EntityManagerFactory;
 import java.io.File;
 
 
@@ -24,11 +29,18 @@ public class ImportConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(ImportConfiguration.class);
 
+    @Autowired
+    private final EntityManagerFactory entityManagerFactory;
+
     @Value("${source.dir.path}")
     private String sourceDirectoryPath;
 
     @Value("${dest.dir.path}")
     private String destDirectoryPath;
+
+    public ImportConfiguration(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
 
     @Bean
     public MessageSource<File> sourceDirectory() {
@@ -40,25 +52,25 @@ public class ImportConfiguration {
     }
 
     @Bean
-    public MessageHandler destDirectory() {
-        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(destDirectoryPath));
-        handler.setExpectReply(false);
-        handler.setDeleteSourceFiles(true);
-
-        return handler;
+    public JpaUpdatingOutboundEndpointSpec jpaPersistHandler() {
+        return Jpa.outboundAdapter(this.entityManagerFactory)
+                .entityClass(Brand.class)
+                .persistMode(PersistMode.PERSIST);
     }
 
     @Bean
     public IntegrationFlow fileMoveFlow() {
         return IntegrationFlows.from(sourceDirectory(), conf -> conf.poller(Pollers.fixedDelay(2000)))
-                .filter(msg ->((File) msg).getName().endsWith(".txt"))
+                .filter(msg -> ((File) msg).getName().endsWith(".txt"))
                 .transform(new FileToStringTransformer())
-                .<String, String>transform(String::toUpperCase)
-                .transform(Message.class, msg ->{
-                    msg.getHeaders().forEach((key, value) -> logger.info("{} : {}", key, value));
-                    return msg;
+                .split(s -> s.delimiters("\n"))
+                .<String, Brand>transform(name -> {
+                    Brand brand = new Brand();
+                    brand.setName(name);
+                    return brand;
                 })
-                .handle(destDirectory())
+                //.handle(destDirectory())
+                .handle(jpaPersistHandler(), ConsumerEndpointSpec::transactional)
                 .get();
     }
 }
