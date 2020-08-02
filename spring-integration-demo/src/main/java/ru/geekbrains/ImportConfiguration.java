6f59bcf927209fe2c70a9ece2f8e5d1ecba1fdc2
AlterMann <MoskaletsAndrey@yandex.ru>
2020-08-02 15:24:40 +0300

    добавил парсинг csv

--- spring-integration-demo/src/main/java/ru/geekbrains/ImportConfiguration.java
+++ spring-integration-demo/src/main/java/ru/geekbrains/ImportConfiguration.java
@@ -1,5 +1,8 @@
 package ru.geekbrains;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +21,15 @@ import org.springframework.integration.file.transformer.FileToStringTransformer;
 import org.springframework.integration.jpa.dsl.Jpa;
 import org.springframework.integration.jpa.dsl.JpaUpdatingOutboundEndpointSpec;
 import org.springframework.integration.jpa.support.PersistMode;
+import org.springframework.integration.transformer.GenericTransformer;
 import ru.geekbrains.model.Brand;
 
 import javax.persistence.EntityManagerFactory;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
@@ -63,14 +71,53 @@ public class ImportConfiguration {
         return IntegrationFlows.from(sourceDirectory(), conf -> conf.poller(Pollers.fixedDelay(2000)))
                 .filter(msg -> ((File) msg).getName().endsWith(".txt"))
                 .transform(new FileToStringTransformer())
-                .split(s -> s.delimiters("\n"))
-                .<String, Brand>transform(name -> {
-                    Brand brand = new Brand();
-                    brand.setName(name);
-                    return brand;
+//                .split(s -> s.delimiters("\n"))
+
+                .<String, Brand>transform(new GenericTransformer<String, List<Brand>>() {
+                    @Override
+                    public List<Brand> transform(String name) {
+//                        Brand brand = new Brand();
+//                        brand.setName(name);
+//                        return brand;
+                        return parseCsv();
+
+                    }
                 })
-                //.handle(destDirectory())
                 .handle(jpaPersistHandler(), ConsumerEndpointSpec::transactional)
                 .get();
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private List<Brand>  parseCsv()  {
+        CsvToBean csv = new CsvToBean();
+        String csvFilename = "F:/User/Andrew/Downloads/data.csv";
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReader(new FileReader(csvFilename));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        List list = csv.parse(setColumnMapping(), csvReader);
+        List<Brand> brands = new ArrayList<>();
+        for (Object object : list) {
+            brands.add((Brand) object);
+        }
+        return brands;
+
+    }
+
+    private Brand castToBrand(Object object) {
+        return (Brand) object;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private static ColumnPositionMappingStrategy setColumnMapping() {
+        ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();
+        strategy.setType(Brand.class);
+        String[] columns = new String[] {"name"};
+        strategy.setColumnMapping(columns);
+        return strategy;
     }
 }
